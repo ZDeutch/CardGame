cb3cdd11cf270ea8ce2b7601c3557c21a4bb7ce8
ZDeutch <zander.deutch@menloschool.org>
2024-11-18 09:39:14 -0800

    Finished the Card Class

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public class Card {
+    private String rank;
+    private String suit;
+    private int value;
+
+    Card(String r, String s, int v) {
+        rank = r;
+        suit = s;
+        value = v;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        return rank + " of " + suit;
+    }
 }
